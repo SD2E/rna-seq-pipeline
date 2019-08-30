e605ebace7b95118ab5cd38f4f749433b5db7a49
eho <eho@tacc.utexas.edu>
2019-08-30 09:50:38 -0500

    Remove old tests

--- mini_qc_rx/scripts/deploy-and-msg.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-abaco deploy -U
-actorId=$(head -1 .ACTOR_ID)
-echo Sleeping for 15... && sleep 15
-execId=$(python scripts/msg-rx.py $actorId)
-abaco executions $actorId
-abaco logs $actorId $execId
-echo $actorId $execId
