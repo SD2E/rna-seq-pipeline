cb9edc8a6b1ab9b5391ed78a4c220f15795dfe05
eho <eho@tacc.utexas.edu>
2019-09-07 12:34:20 -0500

    Migrate get_from_dcuuid and get_datacat_jobs from Jupyter env

--- audit_and_score_rx/reactor.py
+++ audit_and_score_rx/reactor.py
@@ -4,22 +4,72 @@ import os
 from urllib.parse import unquote, urlsplit
 import json
 from inspect import getsource as gs
+from pprint import pprint as pp
 
 
-def submit_agave_job(job_template):
-    try:
-        job_id = r.client.jobs.submit(body=job_template)['id']
-    except Exception as e:
-        r.logger.error("Error submitting job: {}".format(e))
-        if e.response.content is not None:
-            print(e.response.content)
+def get_datacat_jobs(query={}, projection={}, dbURI="", return_max=-1):
+    """Some docs"""
+    # handle defaults
+    if not query:
+        query = { 'pipeline_uuid': "106d3f7f-07dc-596f-86f9-df75083e52cc"}
+    if not dbURI:
+        dbURI='***REMOVED***'
+    # omit projection parameter for all fields
+    args = [query]
+    if projection:
+        args.append(projection)
+    db = pymongo.MongoClient(dbURI).catalog_staging
+    jobs_table = db.jobs
+    results = []
+    i = 1
+    for job in jobs_table.find(*args):
+        results.append(job)
+        if return_max == -1:
+            continue
+        elif i >= return_max:
+            break
         else:
-            print(e)
-    else:
-        r.logger.info("Submitted job {}".format(job_id))
-    finally:
-        print("job_template:")
-        print(json.dumps(job_template, indent=4))
+            i += 1
+    return results
+
+
+def get_from_dcuuid(datacatalog_uuid, extra_fields=[]):
+    """Given `datacatalog_uuid` corresponding to a datacatalog
+    job, return the Tapis job ID. Also returns projected MongoDB
+    response.
+
+    Args:
+        datacatalog_uuid (str): UUID of datacatalog PipelineJob
+        extra_fields (list): list of keynames of additional fields
+        to return in response object e.g. ['archive_system', 'session']
+        Defaults to empty list.
+
+    Returns:
+        tuple: (MongoDB response, Tapis job ID)
+    """
+    # assemble query and projection
+    query = {
+        'uuid': datacatalog_uuid,
+        'history': {'$elemMatch': {'data.launched': {'$exists': True}}}
+    }
+    projection = {
+        'history': {'$elemMatch': {'data.launched': {'$exists': True}}}
+    }
+    # append extra_fields per arg
+    for f in extra_fields:
+        projection[f] = 1
+    # query db
+    datacat_response = get_datacat_jobs(query=query, projection=projection)
+    if not datacat_response:
+        #r.logger.error("Jobs table found no jobs querying against query={}".format(query))
+        return ({}, "")
+    try:
+        tapis_jobId = datacat_response[0]['history'][0]['data']['launched']
+    except (IndexError, KeyError) as e:
+        # really shouldnt happen since query, projection, and indexing are consistent
+        # r.on_failure("Error parsing datacatalog_uuid={}".format(datacatalog_uuid), e)
+        raise e
+    return (datacat_response, tapis_jobId)
 
 
 def dl_from_agave(url):
@@ -67,70 +117,73 @@ def main():
     ag = r.client
     r.logger.debug(json.dumps(r.context, indent=4))
 
-
-    return
-    # Pull attr from message context
-    message_dict = getattr(r.context, 'message_dict', {})
-    fs_remote_fp = unquote(getattr(r.context, 'flagstat_remote_fp', ""))
-    if not message_dict:
-        r.on_failure("Failed to pull Reactor.context.message_dict")
-    if not fs_remote_fp:
-        r.on_failure("Failed to pull Reactor.context.flagstat_remote_fp")
-
-    try:
-        fs_preproc_fp = [f.value.default for f in
-                         ag.apps.get(appId=message_dict['appId'])['outputs']
-                         if f['id'] == "flagstat"][0]
-    except (AttributeError, IndexError) as e:
-        r.on_failure("Failed to pull default flagstat output from " +
-                     "app definition", e)
-    r.logger.info(fs_preproc_fp)
-
-    #r.logger.info(ag.jobs.get(jobId=jobId))
-    try:
-        response = ag.files.download(systemId='data-tacc-work-eho',
-                                     filePath='archive/jobs/eho-mini_preproc_app-test-fdc4716b-9010-4e84-ae0e-089c51021e5e-007/flagstat_out.txt')
-        r.logger.info(response)
-        r.logger.info(type(response))
-        r.logger.info(dir(response))
-        print(response.text)
-        print(response.url)
-        print(response.request.body)
-        print(response.history)
-        print(response.headers)
-        print(dict(response.json()))
-    except Exception as e:
-        print(e)
-    try:
-        r.logger.info(ag.files.downloadFromDefaultSystem())
-    except Exception as e:
-        print(e)
-    try:
-        r.logger.info(ag.files.importData(systemId='data-tacc-work-eho',
-                                          filePath='archive/jobs/eho-mini_preproc_app-test-fdc4716b-9010-4e84-ae0e-089c51021e5e-007/flagstat_out.txt'))
-    except Exception as e:
-        print(e)
-    try:
-        r.logger.info(ag.files.importToDefaultSystem())
-    except Exception as e:
-        print(e)
-    for f in os.listdir("./"):
-        print(f)
-
-    return
-    # Download flagstat file to cwd
-    # fs_remote_fp = "agave://data-sd2e-community/products/v2/106bd127e2d257acb9be11ed06042e68/PAVyR8Dv1evr40LyJ52dX0DP/OZY85OoqyjJ2jZz2JAqLdR0J/sample.ginkgo.13108575.experiment.ginkgo.19606.19637.19708.19709_MG1655_NAND_Circuit_replicate_4_time_18.0:hours_temp_37.0_arabinose_0.5_mM_IPTG_0.00025_mM.rnaseq.original.bwa.flagstat.txt"
-    try:
-        fs_fp = dl_from_agave(fs_remote_fp)
-    except Exception as e:
-        r.on_failure("Failed to download file from {}".format(fs_remote_fp), e)
-    if not fs_fp:
-        r.on_failure("Failed to download file from {}".format(fs_remote_fp))
-
-    r.logger.debug("fs_fp={}".format(fs_fp))
-    # Check file size
-    fs_bytes = path.getsize(fs_fp)
-    r.logger.info("File is {} bytes".format(fs_bytes))
+    (datacat_response, tapis_jobId) = get_from_dcuuid('10779ebc-db12-5eac-9fcf-03deb2cb0c70',
+                                                     ['archive_system', 'archive_path'])
+    print(tapis_jobId)
+    pp(datacat_response)
+    # return
+    # # Pull attr from message context
+    # message_dict = getattr(r.context, 'message_dict', {})
+    # fs_remote_fp = unquote(getattr(r.context, 'flagstat_remote_fp', ""))
+    # if not message_dict:
+    #     r.on_failure("Failed to pull Reactor.context.message_dict")
+    # if not fs_remote_fp:
+    #     r.on_failure("Failed to pull Reactor.context.flagstat_remote_fp")
+    #
+    # try:
+    #     fs_preproc_fp = [f.value.default for f in
+    #                      ag.apps.get(appId=message_dict['appId'])['outputs']
+    #                      if f['id'] == "flagstat"][0]
+    # except (AttributeError, IndexError) as e:
+    #     r.on_failure("Failed to pull default flagstat output from " +
+    #                  "app definition", e)
+    # r.logger.info(fs_preproc_fp)
+    #
+    # #r.logger.info(ag.jobs.get(jobId=jobId))
+    # try:
+    #     response = ag.files.download(systemId='data-tacc-work-eho',
+    #                                  filePath='archive/jobs/eho-mini_preproc_app-test-fdc4716b-9010-4e84-ae0e-089c51021e5e-007/flagstat_out.txt')
+    #     r.logger.info(response)
+    #     r.logger.info(type(response))
+    #     r.logger.info(dir(response))
+    #     print(response.text)
+    #     print(response.url)
+    #     print(response.request.body)
+    #     print(response.history)
+    #     print(response.headers)
+    #     print(dict(response.json()))
+    # except Exception as e:
+    #     print(e)
+    # try:
+    #     r.logger.info(ag.files.downloadFromDefaultSystem())
+    # except Exception as e:
+    #     print(e)
+    # try:
+    #     r.logger.info(ag.files.importData(systemId='data-tacc-work-eho',
+    #                                       filePath='archive/jobs/eho-mini_preproc_app-test-fdc4716b-9010-4e84-ae0e-089c51021e5e-007/flagstat_out.txt'))
+    # except Exception as e:
+    #     print(e)
+    # try:
+    #     r.logger.info(ag.files.importToDefaultSystem())
+    # except Exception as e:
+    #     print(e)
+    # for f in os.listdir("./"):
+    #     print(f)
+    #
+    # return
+    # # Download flagstat file to cwd
+    # # fs_remote_fp = "agave://data-sd2e-community/products/v2/106bd127e2d257acb9be11ed06042e68/PAVyR8Dv1evr40LyJ52dX0DP/OZY85OoqyjJ2jZz2JAqLdR0J/sample.ginkgo.13108575.experiment.ginkgo.19606.19637.19708.19709_MG1655_NAND_Circuit_replicate_4_time_18.0:hours_temp_37.0_arabinose_0.5_mM_IPTG_0.00025_mM.rnaseq.original.bwa.flagstat.txt"
+    # try:
+    #     fs_fp = dl_from_agave(fs_remote_fp)
+    # except Exception as e:
+    #     r.on_failure("Failed to download file from {}".format(fs_remote_fp), e)
+    # if not fs_fp:
+    #     r.on_failure("Failed to download file from {}".format(fs_remote_fp))
+    #
+    # r.logger.debug("fs_fp={}".format(fs_fp))
+    # # Check file size
+    # fs_bytes = path.getsize(fs_fp)
+    # r.logger.info("File is {} bytes".format(fs_bytes))
 
 if __name__ == '__main__':
     main()
