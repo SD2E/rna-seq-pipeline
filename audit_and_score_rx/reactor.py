b14b69c48a872e657632b63f91038f5dd7128eeb
eho <eho@tacc.utexas.edu>
2019-09-13 15:44:19 -0500

    More support for edge cases
    Tapis job resubmission only occurs if both the following are less than
    r.settings.options.max_retries:
    1.) number of *.err files in archivePath
    2.) number of &status=[ FAILED || FINISHED] messages sent to RMPJ

--- audit_and_score_rx/reactor.py
+++ audit_and_score_rx/reactor.py
@@ -8,10 +8,22 @@ import os
 import requests
 from glob import glob
 import pymongo
-#from datacatalog.managers.pipelinejobs import ReactorManagedPipelineJob as Job
 
 
-def ag_jobs_resubmit(tapis_link):
+def require_keys(dict, keys_list):
+    """Return `dict`. r.on_failure unless keys_list is a subset of
+    dict.keys().
+    """
+    k_list = keys_list.copy()
+    for k in k_list:
+        try:
+            _ = dict[k]
+        except KeyError as e:
+            r.on_failure("Error pulling attr from dict", e)
+    return dict
+
+
+def ag_jobs_resubmit(job_self_link):
     """Re-submit an Agave/Tapis job. The job is assigned a new uuid,
     but is submitted with exactly the same definition, parameters, and
     inputs. In the context of this reactor, this means it will also have
@@ -19,31 +31,26 @@ def ag_jobs_resubmit(tapis_link):
     curl -sk -H "Authorization: Bearer $TOKEN" -H "Content-Type:
     application/json" -X POST '`tapis_link`/resubmit'
     """
-    url = os.path.join(tapis_link, "resubmit")
+    url = os.path.join(job_self_link, "resubmit")
     token = agaveutils.get_api_token(r.client)
-    headers = {
-        "Authorization": 'Bearer {}'.format(token),
-        "Content-Type": "application/json"
-    }
-    api_key = ""
-    api_secret = ""
+    headers = {"Authorization": 'Bearer {}'.format(token),
+               "Content-Type": "application/json"}
     response = requests.post(url, headers=headers)
     return response
 
 
-def get_datacat_jobs(query={}, projection={}, dbURI="", return_max=-1):
-    """Some docs"""
+def query_jobs_table(query={}, projection={}, dbURI="", return_max=-1):
+    """MongoDB.find(`query`, `projection`) in SD2E datacatalog. Looks
+    in jobs view of catalog_staging db.
+    """
     # handle defaults
-    if not query:
-        query = { 'pipeline_uuid': "106d3f7f-07dc-596f-86f9-df75083e52cc"}
     if not dbURI:
-        dbURI='***REMOVED***'
-    # omit projection parameter for all fields
+        dbURI = '***REMOVED***'
+    # omit projection parameter if you want all fields returned
     args = [query]
     if projection:
         args.append(projection)
-    db = pymongo.MongoClient(dbURI).catalog_staging
-    jobs_table = db.jobs
+    jobs_table = pymongo.MongoClient(dbURI).catalog_staging.jobs
     results = []
     i = 1
     for job in jobs_table.find(*args):
@@ -55,83 +62,6 @@ def get_datacat_jobs(query={}, projection={}, dbURI="", return_max=-1):
         else:
             i += 1
     return results
-
-
-def get_from_dcuuid(datacatalog_uuid, extra_fields=[]):
-    """Given `datacatalog_uuid` corresponding to a datacatalog
-    job, return the Tapis job ID. Also returns projected MongoDB
-    response.
-
-    Args:
-        datacatalog_uuid (str): UUID of datacatalog PipelineJob
-        extra_fields (list): list of keynames of additional fields
-        to return in response object e.g. ['archive_system', 'session']
-        Defaults to empty list.
-
-    Returns:
-        tuple: (MongoDB response, Tapis job ID)
-    """
-    # assemble query and projection
-    query = {
-        'uuid': datacatalog_uuid,
-        'history': {'$elemMatch': {'data.launched': {'$exists': True}}}
-    }
-    projection = {
-        'history': {'$elemMatch': {'data.launched': {'$exists': True}}}
-    }
-    # append extra_fields per arg
-    for f in extra_fields:
-        projection[f] = 1
-    # query db
-    datacat_response = get_datacat_jobs(query=query, projection=projection)
-    if not datacat_response:
-        r.logger.error("Jobs table found no jobs querying against query={}".format(query))
-        return ({}, "")
-    try:
-        tapis_jobId = datacat_response[0]['history'][0]['data']['launched']
-    except (IndexError, KeyError) as e:
-        # really shouldnt happen since query, projection, and indexing are consistent
-        r.on_failure("Error parsing datacatalog_uuid={}".format(datacatalog_uuid), e)
-        #raise e
-    return (datacat_response[0], tapis_jobId)
-
-
-def dl_from_agave(url):
-    """Given agave-type file path `url`, attempts to download file
-    and returns the path of the copy at cwd.
-
-    Args:
-        url (str): Agave-accessible URL pointing to file e.g.
-        agave://data-tacc-work/folder/subfolder/file.txt
-
-    Returns:
-        str: path to local copy of file
-    """
-    assert type(url) == str
-    url_split = urlsplit(url)
-    filename = url_split[2][url_split[2].rfind("/")+1:]
-    #r.logger.debug("url_split={}".format(url_split))
-    #r.logger.debug("filename={}".format(filename))
-    try:
-        local_fp = agaveutils.agave_download_file(
-            agaveClient=r.client,
-            agaveAbsolutePath=url_split[2],
-            systemId=url_split[1],
-            localFilename=filename)
-    except Exception as e:
-        try:
-            print(e.response.content)
-            assert e.response.content is not None
-        except AssertionError:
-            print("Response is NoneType")
-        except AttributeError:
-            pass
-        finally:
-            r.logger.error(e)
-            print(e)
-            return ""
-    else:
-        return local_fp
 
 
 def validate_archive(dir_path, glob_query, min_mb=1.):
@@ -151,9 +81,9 @@ def validate_archive(dir_path, glob_query, min_mb=1.):
         r.on_failure("Could not find directory at {}".format(dir_path),
                      FileNotFoundError())
         return False
-    else:
-        r.logger.debug("ls {}".format(dir_path))
-        r.logger.debug(glob(dir_path + "/*"))
+    elif not glob(dir_path + "/*.err"):
+        r.logger.warning("No error files found in archive directory" +
+                         format(dir_path))
     for fname in glob_query:
         fp = dir_path + fname
         match = glob(fp)
@@ -171,27 +101,32 @@ def validate_archive(dir_path, glob_query, min_mb=1.):
     return True
 
 
-def new_datacat_job():
-    sample_args = {
-        "experiments": ['experiment.tacc.10001'],
-        "samples": ['sample.tacc.20001'],
-        "measurements1": ['measurement.tacc.0xDEADBEF1'],
-        "measurements2": ['10483e8d-6602-532a-8941-176ce20dd05a', 'measurement.tacc.0xDEADBEF0'],
-        "measurements3": ['measurement.tacc.0xDEADBEEF', 'measurement.tacc.0xDEADBEF0'],
-        "data_w_inputs": {'alpha': 0.5, 'inputs': ['agave://data-sd2e-community/uploads/tacc/example/345.txt'], 'parameters': {'ref1': 'agave://data-sd2e-community/reference/novel_chassis/uma_refs/MG1655_WT/MG1655_WT.fa'}}
+def count_tapis_msg(mpjId):
+    """Counts number of {'status': ''} messages sent to PipelineJob with
+    uuid `mpjId`
+    """
+    query = {
+        'uuid': mpjId,
+        'history': {'$elemMatch': {'data.launched': {'$exists': True}}}
     }
-
-    mpj_kwargs = {
-        "experiment_id": "eho-dummy-id",
-        "data": {
-            "experiment_id": "experiment.tacc.10001"
-        },
-        "archive_patterns": {},
-        "product_patterns": {}
+    projection = {
+        'history': 1 #{'$elemMatch': {'data.launched': {'$exists': True}}}
     }
+    # query db
+    response = query_jobs_table(query=query, projection=projection)
+    if not response:
+        r.logger.error("Jobs table found no jobs querying against query={}".format(query))
+        return int(0)
+    # identify messages from Tapis jobs as ones with {'data': {'status': '*'}}
+    tapis_messages = []
+    for m in response[0]['history']:
+        if type(m.get('data')) != dict:
+            continue
+        elif m['data'].get('status') in ("FINISHED", "FAILED"):
+            # == 'FINISHED' || m.get('name') == 'finish':
+            tapis_messages.append(m)
+    return len(tapis_messages)
 
-    rmpj = Job(r, **mpj_kwargs)
-    print(rmpj)
 
 def main():
     """Main function"""
@@ -200,78 +135,55 @@ def main():
     ag = r.client
     r.logger.debug(json.dumps(r.context, indent=4))
 
-    print(dir(ag.token))
-    print(ag.token.api_key)
-    print(ag.token.api_secret)
-    print(ag.token.refresh)
-    print(agaveutils.get_api_token(ag))
+    # pull from message context and settings
+    msg = require_keys(r.context, ['mpjId', 'tapis_jobId'])
+    settings = require_keys(r.settings.options, ['max_retries', 'work_mount',
+                                                 'min_fastq_mb'])
+    r.logger.info("Validating datacatalog jobId={}".format(msg['mpjId']))
+    # rmpj.validating(data={})
 
-    # pull mpj_Id and tapis_jobId from context
+    # query Tapis against tapis_jobId
     try:
-        datacat_jobId = r.context['mpjId']
-        tapis_jobId = r.context['tapis_jobId']
-        max_retries = r.settings['max_retries']
-    except KeyError as e:
-        r.on_failure("Failed to pull attr from message context", e)
-    r.logger.info("Validating datacatalog jobId={}".format(datacat_jobId))
-    # rmpj.validate(data={})
-
-    # query Tapis against jobId
-    dummy_tapis_jobId = 'a4564989-2393-4b6a-b4e5-598e5a13929f-007'
-    try:
-        tapis_response = ag.jobs.get(jobId=tapis_jobId)
+        tapis_resp = ag.jobs.get(jobId=msg['tapis_jobId'])
     except HTTPError as e:
-        r.on_failure("Error pulling Tapis job", e)
-    try:
-        job_status = tapis_response['status']
-        assert job_status == 'FINISHED'
-        archiveDir = tapis_response['archiveSystem'] + "/" + \
-            tapis_response['archivePath']
-        job_self_link = tapis_response['_links']['self']['href']
-    except KeyError as e:
-        r.on_failure("Could not find attribute", e)
-    except AssertionError as e:
-        r.on_failure("Tapis jobId={} status={}".format(tapis_jobId,
-                                                       job_status))
-    else:
-        r.logger.debug("Tapis jobId={} status={}".format(tapis_jobId,
-                                                         job_status))
-    pp(tapis_response)
-
-    # query the MongoDB jobs table
-    # (datacat_response, _tapis_jobId) = get_from_dcuuid(
-    #     datacat_jobId, ['archive_system', 'archive_path'])
-    # job_dir = '/work/projects/SD2E-Community/prod/data/' + \
-    #     datacat_response['archive_path']
-    # pp(datacat_response)
+        r.on_failure("Error pulling jobId from Tapis API", e)
+    # Make sure we have the necessary keys
+    job = require_keys(tapis_resp, ['archiveSystem', 'archivePath',
+                                    'status', '_links'])
+    # job['_link']['self']['href']
+    job['self_link'] = jobs['_links'].get('self', {}).get('href', '')
 
     # check for existence and size of fastq files
-    # work_mount = '/work/projects/SD2E-Community/prod/data/' + \
-    #     tapis_response['archivePath']
-    work_mount = '/work/06634/eho/' + tapis_response['archivePath']
-    valid = validate_archive(work_mount,
-                        ["/*R1*.fastq.gz", "/*R2*.fastq.gz"],
-                        min_mb=10)
-    if valid:
+    is_valid = validate_archive(settings['work_mount'] + job['archivePath'],
+                                ["/*R1*.fastq.gz", "/*R2*.fastq.gz"],
+                                min_mb=settings['min_fastq_mb'])
+    if job['status'] != 'FINISHED':
+        r.on_failure("Tapis jobId={} {}.".format(
+            msg['tapis_jobId'], job['status']) + "Skipping resubmission.")
+    elif is_valid:
         r.logger.info("Outputs for preprocessing jobId=" +
-                      "{} passed validation".format(tapis_jobId))
-        # rmpj.finished(data={})
+                      "{} passed validation".format(msg['tapis_jobId']))
+        # rmpj.validated(data={})
         # ag.actors.sendMessage(actorId='alignment rx',body={'message':''})
     else:
         r.logger.error("Outputs for preprocessing jobId=" +
-                       "{} failed validation".format(tapis_jobId))
-        # check if max_retries has been met yet
-        err_file_ct = len(glob(work_mount + "/*.err"))
-        if err_file_ct >= max_retries:
+                       "{} failed validation".format(msg['tapis_jobId']))
+        num_tapis_msg = count_tapis_msg(msg['mpjId'])
+        err_file_ct = len(glob(settings['work_mount'] +
+                               job['archivePath'] + "/*.err"))
+        # Only resubmit if the # error files in the archivePath
+        # and Tapis jobs in the datacatalog are both less than max_retries
+        if err_file_ct >= settings['max_retries'] or num_tapis_msg >= settings['max_retries']:
             r.on_failure("Cannot resubmit jobId={}, max_retries={}".format(
-                tapis_jobId, max_retries) + " has been met or exceeded")
+                msg['tapis_jobId'], settings['max_retries']) +
+                " has been met or exceeded")
+            # rmpj.fail(data={})
             return
         else:
             # resubmit Tapis job
-            r.logger.debug("Resubmitting Tapis job")
-            response = ag_jobs_resubmit(tapis_link=job_self_link)
-            print(response)
-        # rmpj.fail(data={})
+            r.logger.info("Resubmitting Tapis jobId={}".format(msg['tapis_jobId']))
+            resubmit_resp = ag_jobs_resubmit(job_self_link=job['self_link'])
+            r.logger.info(resubmit_resp.content)
 
 
 if __name__ == '__main__':
