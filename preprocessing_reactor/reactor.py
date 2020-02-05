f669a7d80bea4e6f3e0236b50a93c9b7aa24db4e
urrutia <jurrutia@tacc.utexas.edu>
2020-02-05 15:11:51 -0600

    automation updates

--- preprocessing_reactor/reactor.py
+++ preprocessing_reactor/reactor.py
@@ -4,6 +4,7 @@ from datacatalog.managers.pipelinejobs import ReactorManagedPipelineJob as Job
 import json
 import pprint
 import copy
+import os
 from datacatalog.tokens import get_admin_token, validate_token
 
 
@@ -11,6 +12,7 @@ def manifest(r):
     ag = r.client  # Agave client
     context = r.context  # Actor context
     m = context.message_dict
+    print(json.dumps(m, indent=4))
 
     # start parsing manfiest
     manifestUrl = m.get('uri')
@@ -37,6 +39,10 @@ def manifest(r):
     if mani_file is None:
         r.on_failure("failed to get manifest {}".format(manifestUrl), e)
 
+    # check for manual override of the manifest path
+    if 'manifestPathOverwrite' in m:
+        dirPath = '/' + m['manifestPathOverwrite']
+
     try:
         manifest = json.load(open(manifestFileName))
     except Exception as e:
@@ -47,8 +53,13 @@ def manifest(r):
     rna_list = []
     for sample in manifest["samples"]:
         mes_types = [measurement["measurement_type"] for measurement in sample["measurements"]]
-        if mes_types[0] == "RNA_SEQ":
-            rna_list.append(sample)
+        if mes_types != []:
+            if mes_types[0] == "RNA_SEQ":
+                rna_list.append(sample)
+
+    if rna_list == []:
+        r.logger.info("No RNA-seq measurements found in manifest: {}".format(manifestUrl))
+        exit(0)
 
     for sample in rna_list:
         sample_id = sample['sample_id']
@@ -79,8 +90,8 @@ def manifest(r):
         inputs = job_def["parameters"]
         #inputs["path_read1"] = '/work/projects/SD2E-Community/prod/data/'+archivePath+'/'+sample['R1'].split('/')[-1]
         #inputs["path_read2"] = '/work/projects/SD2E-Community/prod/data/'+archivePath+'/'+sample['R2'].split('/')[-1]
-        inputs["path_read1"] = '/uploads/ginkgo/201905/NovelChassis-NAND2-0-Omics/' + sampleDict[sample_id]['R1']
-        inputs["path_read2"] = '/uploads/ginkgo/201905/NovelChassis-NAND2-0-Omics/' + sampleDict[sample_id]['R2']
+        inputs["path_read1"] = os.path.normpath(dirPath + '/' + sampleDict[sample_id]['R1']).replace('//','/')
+        inputs["path_read2"] = os.path.normpath(dirPath + '/' + sampleDict[sample_id]['R2']).replace('//','/')
         inputs["multiple_lanes"] = sampleDict[sample_id]['multiple_lanes']
         #archivePath = archivePath + "/miniaturized_library_prep/"
         job_def.parameters = inputs
@@ -133,6 +144,15 @@ def manifest(r):
         r.logger.info("Submitted Pipeline Job {}".format(mpj.uuid))
         job_def.archivePath = mpj.archive_path
         #job_def.archivePath = 'shared-q1-workshop/urrutia/experiment.ginkgo.23121_Novel-chassis_2.0_DNA-seq_strain_verfication/preprocessed/'
+        try:
+            audit_nonce = os.getenv('_REACTOR_AUDIT_RNASEQ_NONCE')
+            pipeline_config = copy.copy(r.settings.pipelines)
+            audit_rnaseq_id = pipeline_config['audit_rnaseq_id']
+            api_server = pipeline_config['api_server']
+            audit_rnaseq_callback = api_server + '/actors/v2/' + audit_rnaseq_id + '/messages?x-nonce=' + audit_nonce
+        except Exception as e:
+            print(e)
+            r.logger.error("Unable to generate Audit callback")
 
         notif = [{'event': 'RUNNING',
                   "persistent": True,
@@ -142,7 +162,12 @@ def manifest(r):
                   'url': mpj.callback + '&status=${JOB_STATUS}'},
                  {'event': 'FINISHED',
                   "persistent": False,
-                  'url': mpj.callback + '&status=FINISHED'}]
+                  'url': mpj.callback + '&status=FINISHED'},
+                  {'event': 'FINISHED',
+                   "persistent": False,
+                   'url': audit_rnaseq_callback + '&status=FINISHED' +
+                   '&analysis_type=preprocessing' +
+                   '&mpjId=' + mpj.uuid}]
 
         #notifications = job_template["notifications"]
         #if notifications is not None:
@@ -150,19 +175,26 @@ def manifest(r):
         #       notif.append(item)
 
         job_def["notifications"] = notif
-        print(json.dumps(job_def, indent=4))
+        mpj_state = get_job_state(mpj.uuid, ag)
         try:
-            job_id = ag.jobs.submit(body=job_def)['id']
-            print(json.dumps(job_def, indent=4))
-            mpj.run({"launched": job_id, "experiment_id": experiment_id, "sample_id": sample_id})
-            r.logger.info("Submitted Agave job {}".format(job_id))
+            if mpj_state in ['CREATED', 'RESET']:
+                job_id = ag.jobs.submit(body=job_def)['id']
+                print(json.dumps(job_def, indent=4))
+                r.logger.info("Submitted Agave job {}".format(job_id))
+                mpj.run({"launched": job_id, "sample_id": sample_id, "experiment_id": experiment_id})
+            else:
+                r.logger.info("Unable to submit job, in state {}".format(mpj.state))
         except Exception as e:
             print(json.dumps(job_def, indent=4))
             r.logger.error("Error submitting job: {}".format(e))
             print(e.response.content)
 
 
-
+def get_job_state(id, ag):
+    from datacatalog.managers.pipelinejobs.jobmanager import JobManager
+    mongodb={'authn': os.getenv('_REACTOR_MONGODB_AUTHN'), 'database': 'catalog_staging'}
+    mjob = JobManager(mongodb, agave=ag).load(id)
+    return mjob.state
 
 def main():
     """Main function"""
